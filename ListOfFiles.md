000157be516a06189223e095b52863f1c948599e
AshishSondagar <Candy128x>
2019-05-16 06:23:47 +0530

    controller-dir </> Thu, 2019-05-16, 06:23:46 IST 136

--- ListOfFiles.md
+++ ListOfFiles.md
@@ -7,3 +7,8 @@
 - form1.html
 - expression-1.html
 - directives.html
+- expression_ng-click.html
+
+[controllers dir]
+- controller.html
+- static/js/main.js
---
+++ code/controllers/static/js/main.js
@@ -0,0 +1,5 @@
+var app = angular.module("MyModule", []);
+
+app.controller("MyController", function ($scope) {
+	$scope.message = "Hey dude msg come from controllers.. ;)"
+});
